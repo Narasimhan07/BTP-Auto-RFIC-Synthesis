c388a1ed9a0e52abba146f0a3e35aea9404d3394
narasimhan <narasrikanth2002@gmail.com>
2024-06-06 16:17:01 +0530

    commit: switch-width-in-gd branch made edits to VM_call_optimization_functions.py

--- VM_call_optimization_functions.py
+++ VM_call_optimization_functions.py
@@ -202,10 +202,10 @@ def get_post_optimization_simulation_parameters(VM_passive_mixer):
 
 def get_hand_calculated_circuit_parameters(VM_passive_mixer):
     VM_passive_mixer['hand_calculated_circuit_parameters'] = {}
-    #vm_hc.hand_calculation(VM_passive_mixer['output_conditions'],VM_passive_mixer['hand_calculated_circuit_parameters'])
-    VM_passive_mixer['hand_calculated_circuit_parameters']['res_w'] = 30
-    VM_passive_mixer['hand_calculated_circuit_parameters']['cap_w'] = 20
-    VM_passive_mixer['hand_calculated_circuit_parameters']['sw_mul'] = 30
+    vm_hc.hand_calculation(VM_passive_mixer['output_conditions'],VM_passive_mixer['hand_calculated_circuit_parameters'])
+    #VM_passive_mixer['hand_calculated_circuit_parameters']['res_w'] = 30
+    #VM_passive_mixer['hand_calculated_circuit_parameters']['cap_w'] = 20
+    #VM_passive_mixer['hand_calculated_circuit_parameters']['sw_mul'] = 30
 # END of get_hand_calculated_circuit_parameters()
 
 def set_loss_weights(VM_passive_mixer):
